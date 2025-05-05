f8121c9deedbd290e76ca2f11bfb3eef7a18e701
Md-Roni024 <roni.cse024@gmail.com>
2025-05-05 10:24:28 +0600

    Remove API KEY

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAEWFK-YhDIPrWwx_gm3PVKyZ6mAyPVTcM",
-    authDomain: "readme-apps.firebaseapp.com",
-    projectId: "readme-apps",
-    storageBucket: "readme-apps.firebasestorage.app",
-    messagingSenderId: "359149125968",
-    appId: "1:359149125968:web:75877de85d518cd9ceec55"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 };
 
